e1c038877293eb920c7ab00eaec06976c4f4867d
noeltz <noeltz@web.de>
2026-06-25 10:18:11 +0200

    update

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-INSTALLER_DIR="void-installer"
+INSTALLER_DIR="n0_void-installer"
 
 source ./base.sh
 source ./post.sh
 
 clear
 separator -iwi
-figlet "VOID Linux Installer" 
+figlet "VOID Linux Installer"
 separator -iwi
 sleep 2
 
 listDisk				# List connected disks
-diskFormat				# Format disks and set up partitions + BTRFS 
+diskFormat				# Format disks and set up partitions + BTRFS
 installSystem				# Install the base system
-mkdir -p /mnt/void-installer		# Create and copy scripts for installation inside the CHROOT
-cp -r * /mnt/void-installer/
+mkdir -p /mnt/$INSTALLER_DIR		# Create and copy scripts for installation inside the CHROOT
+cp -r * /mnt/$INSTALLER_DIR/
 
 clear
 separator -iwi
@@ -23,4 +23,4 @@ figlet "Entering CHROOT"
 separator -iwi
 sleep 2
 
-xchroot /mnt /bin/bash -c 'cd /void-installer; ./chroot.sh'
+xchroot /mnt /bin/bash -c "cd /$INSTALLER_DIR; ./chroot.sh"
